d384161e605898929c6ee12a88551b8f9494554f
HRiver <sanscout@naver.com>
2023-12-07 20:02:58 +0900

    20231207

---
+++ src/control/test1.java
@@ -0,0 +1,27 @@
+package control;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a>b || a>c){
+            System.out.printf("1");
+        } else {
+            System.out.printf("0");
+        }
+
+        if(a==b && b==c){
+            System.out.printf(" 1");
+        } else{
+            System.out.printf("0");
+        }
+
+
+    }
+}
