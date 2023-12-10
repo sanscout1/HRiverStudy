52b992c05ea07ccdc37eb7b521b5291033ce3e97
HRiver <sanscout@naver.com>
2023-12-10 15:50:14 +0900

    loop assignment complete

---
+++ src/HR_assignment3/Loop9058_ex04.java
@@ -0,0 +1,20 @@
+package HR_assignment3;
+
+import java.util.Scanner;
+
+public class Loop9058_ex04 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int repeatNum = sc.nextInt();
+        String tmpStar ="*";
+        String outputStar =tmpStar;
+        for(int i=0;i<repeatNum;i++){
+            for(int j=0;j<repeatNum-1-i;j++){
+                System.out.printf(" ");
+            }
+
+            System.out.printf("%s\n",outputStar);
+            outputStar+=tmpStar;
+        }
+    }
+}
