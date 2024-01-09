4913754dbe54bee79fd8962cc888357b1cd0c32e
HRiver <sanscout@naver.com>
2024-01-10 01:10:38 +0900

    finish lamdaquiz

--- src/Java_Lecture/About_Lamda/LamdaQuiz/LamdaQuiz.java
+++ src/Java_Lecture/About_Lamda/LamdaQuiz/LamdaQuiz.java
@@ -1,9 +1,18 @@
-package Java_Lecture.About_Lamda;
+package Java_Lecture.About_Lamda.LamdaQuiz;
 
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+
+interface MathOperation1 {
+    int addTwoNum(int a,int b);
+}
+
+interface Calculator {
+    int calculate(int a,int b);
+}
+
+
 
 public class LamdaQuiz {
     public static void main(String[] args) {
@@ -48,6 +57,33 @@ public class LamdaQuiz {
 
         int multotal = mulIntegerTotal.apply(integerList);
         System.out.println(multotal);  // 1~ 10 곱
+
+        // 7. 람다 식과 스트림을 사용하여 정수 리스트의 평균을 구하는 프로그램 작성
+        double avg=integerList.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        System.out.println("문제 7번");
+        System.out.println(avg);
+
+        //8. 두 개의 정수를 매개 변수로 받아 그 합계를 반환하는 계산이라는 메서드가 있는
+        // MathOperation 이라는 함수형 인터페이스를 만듭니다.
+        MathOperation1 mathOperation1 = (tmp1,tmp2) -> tmp1+tmp2;
+        System.out.println("문제 8번");
+        System.out.println(mathOperation1.addTwoNum(4,8));;
+
+        // 9. 정수를 반환하는 calculate(int a , int b ) 이라는 메서드가 있는 Caluculator라는
+        // 함수형 인터페이스를 만듭니다. 두 숫자를 더하고 그 합을 반환하는 함수형 인터페이스를
+        // 구현하는 람다 표현식 만들기
+        Calculator tmpCal = Integer::sum;
+        System.out.println("문제 9번");
+        System.out.println(tmpCal.calculate(1,4));
+
+
+        //10. 필터,매핑 , 축소 연산을 사용하여 정수 리스트에서 모든 짝수의 총합을 구하세요
+        int total2=integerList.stream().mapToInt(Integer::intValue).filter(i -> i%2==0).sum();
+
+        System.out.println("문제 10번");
+        System.out.println(total2);
+
+
 
     }
 }
---
+++ src/Java_Lecture/About_Lamda/LamdaQuiz/LamdaQuiz03.java
@@ -0,0 +1,25 @@
+package Java_Lecture.About_Lamda.LamdaQuiz;
+
+@FunctionalInterface
+ interface Operator {
+    public int apply(int x, int y);
+}
+
+public class LamdaQuiz03 {
+    private static int[] scores ={10,50,3};
+
+    public static int maxOrMin(Operator operator){
+        int result = scores[0];
+        for (int score : scores) {
+            result = operator.apply(result,score);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int max = maxOrMin((x, y) -> Math.max(x, y));
+        System.out.println("최대값: " + max);
+        int min = maxOrMin(Math::min);
+        System.out.println("최소값: "+ min);
+    }
+}
