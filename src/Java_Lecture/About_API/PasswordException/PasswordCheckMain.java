e3c7434cf6a2f477bf1627ed4c797060ef599483
HRiver <sanscout@naver.com>
2024-01-14 15:21:02 +0900

    update exception note

--- src/Java_Lecture/About_API/PasswordException/PasswordCheckMain.java
+++ src/Java_Lecture/About_API/PasswordException/PasswordCheckMain.java
@@ -12,6 +12,7 @@ public class PasswordCheckMain {
     }
 
     //우리가 원하는 예외 처리 메세지를 저장 할 수 있다.
+    // throw 와 thorws 이용 장소 차이 있음
     public void checkPassword(String password) throws PasswordCheckException {
         if(password == null) {
             throw new PasswordCheckException("비밀면호 입력이 없습니다.");
