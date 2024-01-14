98e460b9ef867a8c7aa7d4f44a7f3b64a03fd741
HRiver <sanscout@naver.com>
2024-01-14 15:24:36 +0900

    update board exception

--- src/Java_Lecture/About_API/exceptionEx.java
+++ src/Java_Lecture/About_API/exceptionEx.java
@@ -1,5 +1,7 @@
 package Java_Lecture.About_API;
 
+import Java_Lecture.About_API.PasswordException.PasswordCheckException;
+
 import java.util.Scanner;
 
 public class exceptionEx {
@@ -11,8 +13,23 @@ public class exceptionEx {
         for (int i = 0; i < 6; i++) {
             array[i]=in.nextInt();
         }
+//            if(password == null) {
+//                throw new PasswordCheckException("비밀면호 입력이 없습니다.");
+//            } else if (password.length() < 6) {
+//                throw new PasswordCheckException("비밀번호가 5자 이하 입니다");
+//
+//            } else if (!password.matches("[a-zA-Z0-9]*")) {
+//                throw new PasswordCheckException("옳지 않은 비밀번호 입니다");
+//            }else {
+//                System.out.println("옳은 비밀번호 입니다.");
+//                this.password=password;
+//            }
         } catch(ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
+//        catch (PasswordCheckException e)
+//        {
+//
+//        }
     }
 }
